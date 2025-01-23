5bd98819062917d52b2ae2008149b874a95afeed
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-01-23 13:45:56 -0500

    tidy

--- tachyon/test.py
+++ tachyon/test.py
@@ -3,15 +3,9 @@ from chemistry import Rule, Mixture, System
 
 
 # %sig: A@100(p[a1.P$m a2.P$m a3.P$m], l[r.A$w], r[l.A]), P@100(a1[p.A], a2[p.A], a3[p.A], d[d.P$m])
-# A.l <-> A.r @ w
-# A.p <-> P.a1 @ m
-# A.p <-> P.a2 @ m
-# A.p <-> P.a3 @ m
-# P.d <-> P.d @ m
-
-
 agent_types = [AgentType("A", ["p", "l", "r"]), AgentType("P", ["a1", "a2", "a3", "d"])]
 rules = [Rule(("A", "A"), ("l", "r"), 1)]
+
 A0 = P0 = 2
 mixture = Mixture(
     [Molecule.create([agent_types[0]]) for _ in range(A0)]
@@ -20,5 +14,5 @@ mixture = Mixture(
 system = System(mixture, rules)
 
 while True:
-    print(system.mixture)
     system.update()
+    print([len(molecule) for molecule in system.mixture])
