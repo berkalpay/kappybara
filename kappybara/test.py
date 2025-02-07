b129e9f9a41a5535164e76cd61b8b85b1fa2babd
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-02-07 10:54:17 -0500

    adjust test and update following ead16443f60e5ab69e634c07a0a351246e245fd2

--- kappybara/test.py
+++ kappybara/test.py
@@ -4,15 +4,23 @@ from chemistry import Rule, Mixture, System
 
 # %sig: A@100(p[a1.P$m a2.P$m a3.P$m], l[r.A$w], r[l.A]), P@100(a1[p.A], a2[p.A], a3[p.A], d[d.P$m])
 agent_types = [AgentType("A", ["p", "l", "r"]), AgentType("P", ["a1", "a2", "a3", "d"])]
-rules = [Rule(("A", "A"), ("l", "r"), 1)]
+rules = [
+    Rule(("A", "A"), ("l", "r"), True, 1),
+    Rule(("A", "A"), ("l", "r"), False, 1),
+    Rule(("P", "P"), ("d", "d"), True, 1),
+    Rule(("P", "P"), ("d", "d"), False, 1),
+    Rule(("P", "a"), ("a1", "p"), True, 1),
+    Rule(("P", "a"), ("a1", "p"), False, 1),
+]
 
-A0 = P0 = 2
+A0 = P0 = 10**4
 mixture = Mixture(
     [Molecule.create([agent_types[0]]) for _ in range(A0)]
     + [Molecule.create([agent_types[1]]) for _ in range(P0)]
 )
 system = System(mixture, rules)
 
-while True:
-    print([len(molecule) for molecule in system.mixture])
+for _ in range(1000):
+    molecule_sizes = [len(molecule) for molecule in system.mixture]
+    print(max(molecule_sizes))
     system.update()
