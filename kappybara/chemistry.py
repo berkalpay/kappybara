ead16443f60e5ab69e634c07a0a351246e245fd2
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-02-07 10:31:23 -0500

    refactor rule applications to not explicitly enumerate all possibilities

--- kappybara/chemistry.py
+++ kappybara/chemistry.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from random import expovariate, choices
-from itertools import chain, product
-from typing import Hashable, NamedTuple, Iterable
+from random import expovariate, choice, choices
+from itertools import chain
+from typing import Hashable, Iterable, Optional
 
 from physics import Site, Agent, Molecule
 
@@ -57,7 +57,10 @@ class Mixture:
                 chain.from_iterable(agent.free_sites for agent in self.agents),
                 lambda site: site.label,
             )
-            self._free_sites = {k: set(free_sites_lists[k]) for k in free_sites_lists}
+            self._free_sites = {
+                site_label: set(free_sites_lists[site_label])
+                for site_label in free_sites_lists
+            }
         return self._free_sites
 
     def free_site(self, site: Site) -> None:
@@ -71,12 +74,50 @@ class Mixture:
 class Rule:
     agent_types: tuple[str, str]  # TODO: unneeded?
     site_labels: tuple[str, str]
+    bind: bool
     rate: float
 
+    def reactivity(self, mixture: Mixture) -> float:
+        return self.n_embeddings(mixture) * self.rate
 
-Action = NamedTuple(
-    "Action", [("sites", tuple[Site]), ("bind", bool), ("activity", float)]
-)  # TODO: refactor
+    def n_embeddings(self, mixture: Mixture) -> int:
+        if self.bind:
+            site1_choices = mixture.free_sites[self.site_labels[0]]
+            site2_choices = mixture.free_sites[
+                self.site_labels[0]
+            ]  # TODO: this isn't quite right
+            return len(site1_choices) * len(site2_choices)
+        else:
+            site_choices = []
+            for agent in mixture.agents_by_type[self.agent_types[0]]:
+                site = agent.interface[self.site_labels[0]]
+                if site.bound and site.partner.label == self.site_labels[1]:
+                    site_choices.append(site)
+            return len(site_choices)
+
+    def _select(self, mixture: Mixture) -> tuple[Site, Optional[Site]]:
+        if self.bind:
+            site1 = choice(tuple(mixture.free_sites[self.site_labels[0]]))
+            site2_choices = tuple(mixture.free_sites[self.site_labels[1]])
+            site2 = site1
+            while site2 is site1:
+                site2 = choice(site2_choices)
+            return (site1, site2)
+        else:
+            site_choices = []
+            for agent in mixture.agents_by_type[self.agent_types[0]]:
+                site = agent.interface[self.site_labels[0]]
+                if site.bound and site.partner.label == self.site_labels[1]:
+                    site_choices.append(site)
+            site_choice = choice(site_choices)
+            return (site_choice, None)
+
+    def act(self, mixture: Mixture) -> None:
+        site1, site2 = self._select(mixture)
+        if self.bind:
+            site1.bind(site2)
+        else:
+            site1.unbind()
 
 
 @dataclass
@@ -87,56 +128,19 @@ class System:
     time: float = 0
 
     @property
-    def possible_unbonds(self) -> list[Action]:
-        actions = []
-        for rule in self.rules:
-            for agent in self.mixture.agents_by_type[rule.agent_types[0]]:
-                agent1_site = agent.interface[rule.site_labels[0]]
-                # Assumes site labels are unique across agent types
-                if (
-                    agent1_site.bound
-                    and agent1_site.partner.label == rule.site_labels[1]
-                ):
-                    actions.append(
-                        Action((agent1_site, agent1_site.partner), False, rule.rate)
-                    )  # TODO: stochastic rate, unimolecular
-        return actions
-
-    @property
-    def possible_bonds(self) -> list[Action]:
-        actions = []
-        for rule in self.rules:
-            sites1 = self.mixture.free_sites[rule.site_labels[0]]
-            sites2 = self.mixture.free_sites[rule.site_labels[1]]
-            for site1, site2 in product(sites1, sites2):
-                if site1.agent is not site2.agent:
-                    actions.append(Action((site1, site2), True, rule.rate))
-        return actions
-
-    @property
-    def possible_actions(self) -> list[Action]:
-        return self.possible_unbonds + self.possible_bonds
+    def rule_reactivities(self) -> list[float]:
+        return [rule.reactivity(self.mixture) for rule in self.rules]
 
     @property
     def reactivity(self) -> float:
-        return sum(action.activity for action in self.possible_actions)
+        return sum(self.rule_reactivities)
 
     def wait(self) -> None:
         self.time += expovariate(self.reactivity)
 
-    def action(self) -> Action:
-        possible_actions = self.possible_actions
-        return choices(
-            possible_actions,
-            weights=[action.activity / self.reactivity for action in possible_actions],
-        )[0]
-
     def act(self) -> None:
-        action = self.action()
-        if action.bind:
-            action.sites[0].bind(action.sites[1])
-        else:
-            action.sites[0].unbind()
+        rule = choices(self.rules, weights=self.rule_reactivities)[0]
+        rule.act(self.mixture)
         self.mixture.molecules = [
             molecule for molecule in self.mixture.molecules if len(molecule)
         ]
