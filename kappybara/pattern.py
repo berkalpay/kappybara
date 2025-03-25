3e655822d235761b199051249bc338180532fd4c
amber227 <jyulin2000@protonmail.com>
2025-03-24 20:49:31 -0500

    pattern: scaffold Pattern instantiation

--- kappybara/pattern.py
+++ kappybara/pattern.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from collections import defaultdict
 from functools import cached_property
-from typing import Self, List
+from typing import Self, List, Dict
 
 from kappybara.grammar import kappa_parser
 from kappybara.site_states import *
-from kappybara.physics import Site, Agent
+from kappybara.physics import Site, Agent, Mixture
 from lark import ParseTree, Visitor, Tree, Token
 
 
@@ -13,11 +14,12 @@ class SitePattern:
     label: str
     internal_state: "InternalStatePattern"
     link_state: "LinkStatePattern"
+    agent: "AgentPattern" = None
 
     def create_instance(self) -> Site:
         assert (
             not self.underspecified
-        ), "Site pattern is not specific enough to be instantiated"
+        ), f"Site pattern: {self} is not specific enough to be instantiated in a mixture"
 
         raise NotImplementedError
 
@@ -27,7 +29,7 @@ class SitePattern:
         Tells you whether or not concrete `Site` instances can be created
         from this pattern, i.e. whether there are ambiguous site states
         """
-        match (internal_state, link_state):
+        match (self.internal_state, self.link_state):
             case (
                 (WildCardPredicate(), _)
                 | (_, WildCardPredicate())
@@ -44,9 +46,9 @@ class SitePattern:
 
         builder = SitePatternBuilder(tree)
         return cls(
-            builder.parsed_site_name,
-            builder.parsed_internal_state,
-            builder.parsed_link_state,
+            label=builder.parsed_site_name,
+            internal_state=builder.parsed_internal_state,
+            link_state=builder.parsed_link_state,
         )
 
 
@@ -75,7 +77,12 @@ class AgentPattern:
         assert tree.data == "agent"
         builder = AgentPatternBuilder(tree)
 
-        return cls(builder.parsed_type, builder.parsed_interface)
+        agent = cls(builder.parsed_type, builder.parsed_interface)
+
+        for site in agent.sites:
+            site.agent = agent
+
+        return agent
 
     @classmethod
     def from_kappa(cls, kappa_str: str) -> Self:
@@ -105,6 +112,10 @@ class MoleculePattern:
     methods for pattern matching/isomorphism; one concrete consideration is
     w.r.t. rectangular approximation and the necessary computations for
     observable patterns. Will try to elaborate later.
+
+    NOTE(24-03-2025): Some new considerations following convo w/ Walter, elaborate.
+    - Optionally turning off connected component tracking
+    - Cost of detailed structs when it comes to FFI conversions
     """
 
     pass
@@ -113,12 +124,64 @@ class MoleculePattern:
 @dataclass
 class Pattern:
     agents: List[AgentPattern]
+    connected_components: List[List[AgentPattern]]
+
+    def __init__(self, agents: List[AgentPattern]):
+        """
+        Compile a pattern from a list of `AgentPatterns` whose edges are implied by integer
+        link states. Replaces integer link states with references to actual partners, and
+        divides up agents into their respective connected components.
+        """
+        self.agents = agents
+        self.connected_components = []
+
+        integer_links: defaultdict[int, list[SitePattern]] = defaultdict(list)
+
+        # Parse out site connections implied by integer LinkStates
+        # TODO: probs do a @property method that directly gives an iterable over all sites like in `physics.py`
+        for agent in self.agents:
+            for site in agent.sites:
+                if isinstance(site.link_state, int):
+                    if site.link_state in integer_links:
+                        integer_links[site.link_state].append(site)
+                    else:
+                        integer_links[site.link_state] = [site]
+
+        # Replace integer LinkStates with AgentPattern references
+        for i in integer_links:
+            linked_sites = integer_links[i]
+            match len(linked_sites):
+                case n if n == 1:
+                    raise AssertionError(
+                        f"Site link {i} is only referenced in one site."
+                    )
+                case n if n > 2:
+                    raise AssertionError(
+                        f"Site link {i} is referenced in more than two sites."
+                    )
+                case n if n == 2:
+                    linked_sites[0].link_state = linked_sites[1]
+                    linked_sites[1].link_state = linked_sites[0]
+
+        # Discover connected components
+        # NOTE: some redundant traversals but prioritized simplicity of code.
+        not_seen: Set[AgentPattern] = set(agents)
+
+        while not_seen:
+            # Pop from not_seen and DFS from it
+            raise NotImplementedError
 
     def create_instance(self) -> Site:
         assert (
             not self.underspecified
         ), "Pattern is not specific enough to be instantiated"
 
+        raise NotImplementedError
+
+    def matches(self, mixture: Mixture):
+        raise NotImplementedError
+
+    def find_all_matches(self, mixture: Mixture):
         raise NotImplementedError
 
     @cached_property
@@ -130,9 +193,13 @@ class Pattern:
         assert tree.data == "pattern"
 
         builder = PatternBuilder(tree)
-        return cls(
-            builder.parsed_agents[::-1]
-        )  # The agents get parsed in reverse order
+
+        # Reassign agent id's so they are unique (in the
+        # context of this individual Pattern)
+        for i in range(len(builder.parsed_agents)):
+            builder.parsed_agents[i].id = i
+
+        return cls(agents=builder.parsed_agents)
 
     @classmethod
     def from_kappa(cls, kappa_str: str) -> Self:
@@ -173,12 +240,14 @@ class SitePatternBuilder(Visitor):
             case str(internal_state):
                 # TODO: check if this is a legal option as specified by the agent signature
                 # this object would need to hold the agent type this site is being built for and do some checks against that
+                # NOTE: Actually probably ignore the above. According to Walter Kappa models shouldn't require explicitly
+                # declared agent signatures in the first place.
                 self.parsed_internal_state = str(internal_state)
             case Tree(data="unspecified"):
                 self.parsed_internal_state = UndeterminedState()
             case _:
                 raise ValueError(
-                    f"Unexpected internal state in Lark's parse tree: {tree}"
+                    f"Unexpected internal state in site parse tree: {tree}"
                 )
 
     # Visitor method for Lark
@@ -202,7 +271,7 @@ class SitePatternBuilder(Visitor):
             case [Tree(data="unspecified")]:
                 self.parsed_link_state = UndeterminedState()
             case _:
-                raise ValueError(f"Unexpected link state in Lark's parse tree: {tree}")
+                raise ValueError(f"Unexpected link state in site parse tree: {tree}")
 
 
 @dataclass
@@ -235,10 +304,13 @@ class PatternBuilder(Visitor):
     def __init__(self, tree: ParseTree):
         super().__init__()
 
-        self.parsed_agents: List[SitePattern] = []
+        self.parsed_agents: List[AgentPattern] = []
 
         assert tree.data == "pattern"
         self.visit(tree)
+
+        # The agents get visited in reverse order
+        self.parsed_agents.reverse()
 
     # Visitor method for Lark
     def agent(self, tree: ParseTree):
