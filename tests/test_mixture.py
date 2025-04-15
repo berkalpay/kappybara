d65aa633f3b5ef90929394174db84fe7a003c480
amber227 <jyulin2000@protonmail.com>
2025-04-15 02:36:47 -0500

    mixture: add test module (could be more thorough)

--- tests/test_mixture.py
+++ tests/test_mixture.py
@@ -30,17 +30,29 @@ def test_instantiate_pattern_one_component(test_str):
         ("A(a1[1]), A(a1[1]), A(a1[.]), A(a1[.])", 1, "A(a1[_])", 2),
         ("A(a1[1]), A(a1[1]), A(a1[.]), A(a1[.])", 1, "A(a1[.])", 2),
         ("A(a1[1]), A(a1[1]), A(a1[.]), A(a1[.])", 1, "A(a1[#])", 4),
+        (
+            "A(a1[1], a2[2], a3[5]), B(b1[2], b2[3]), C(c1[3], c2[4], c3[5]), D(d1[4], d2[1])",
+            1,
+            "A(a1[1], a2[2], a3[5]), B(b1[2], b2[3]), C(c1[3], c2[4], c3[5]), D(d1[4], d2[1])",
+            1,
+        ),
     ],
 )
-def test_find_embeddings(test_case):
+def test_find_embeddings_one_component(test_case):
+    """
+    Test embeddings of patterns consisting of a single component
+    """
+    n = 1000
+
     mixture_pattern_str, n_copies, match_pattern_str, n_embeddings_expected = test_case
     mixture_pattern = Pattern.from_kappa(mixture_pattern_str)
 
     mixture = Mixture()
-    mixture.instantiate(mixture_pattern, n_copies)
+    for i in range(n):
+        mixture.instantiate(mixture_pattern, n_copies)
 
     match_pattern = Pattern.from_kappa(match_pattern_str)
     assert len(match_pattern.components) == 1
 
     embeddings = mixture.find_embeddings(match_pattern.components[0])
-    assert len(embeddings) == n_embeddings_expected
+    assert len(embeddings) == n_embeddings_expected * n
