59581bc477963e6664cd8a0ca47903995a6e0086
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-04-24 15:46:12 -0400

    module edges with mixtures

--- kappybara/mixture.py
+++ kappybara/mixture.py
@@ -5,7 +5,6 @@ from copy import deepcopy
 from warnings import warn
 
 from kappybara.site_states import *
-from kappybara.edge import Edge
 from kappybara.pattern import (
     SitePattern,
     AgentPattern,
@@ -15,6 +14,42 @@ from kappybara.pattern import (
     Agent,
     Component,
 )
+
+
+def cantor(x: int, y: int) -> int:
+    """
+    https://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
+
+    TODO: this has some failure cases due to integer overflow.
+    """
+    return ((x + y) * (x + y + 1)) // 2 + y
+
+
+@dataclass
+class Edge:
+    """
+    Basically, we want a data structure to represent bonds that we can use as keys
+    to a set/dict (i.e. hashset/hashmap). The key property we need is that its hash
+    representation should be unordered: Edge(site1=x, site2=y) represents the same
+    thing as Edge(site1=y, site2=x), so they should function as the same key.
+    """
+
+    site1: SitePattern
+    site2: SitePattern
+
+    def __eq__(self, other: Self):
+        return (self.site1 == other.site1 and self.site2 == other.site2) or (
+            self.site1 == other.site2 and self.site2 == other.site1
+        )
+
+    def __hash__(self):
+        """
+        TODO: Right now XOR is being used to symmetrically combine hashes of the two sites
+        so that we get an unordered hash. This is unsound however: https://stackoverflow.com/a/27952689
+        """
+        return cantor(hash(self.site1), hash(self.site1.agent)) ^ cantor(
+            hash(self.site2), hash(self.site2.agent)
+        )
 
 
 @dataclass
