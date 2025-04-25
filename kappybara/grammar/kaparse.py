36f2cd6e4e8ea6fa012c023d0f564a9faa48fd08
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-04-25 09:44:24 -0400

    tidy kaparse

--- kappybara/grammar/kaparse.py
+++ kappybara/grammar/kaparse.py
@@ -3,34 +3,27 @@ from lark import Lark, ParseTree
 
 
 class KappaParser:
-    """
-    Don't instantiate this class, use `kappa_parser`, which
-    is provided by the top level of the `grammar` module
-    """
+    """Don't instantiate: use `kappa_parser`"""
 
     def __init__(self) -> None:
-        lark_file_path = str(Path(__file__).parent / "kappa.lark")
-
         self._parser = Lark.open(
-            lark_file_path,
+            str(Path(__file__).parent / "kappa.lark"),
             rel_to=__file__,
-            #  parser='lalr',
             parser="earley",
-            # Using the basic lexer isn't required, and isn't usually recommended.
+            # The basic lexer isn't required and isn't usually recommended
             lexer="dynamic",
-            #  lexer='basic',
             start="kappa_input",
-            # Disabling propagate_positions and placeholders slightly improves speed
+            # Disabling these slightly improves speed
             propagate_positions=False,
             maybe_placeholders=False,
         )
 
+    def parse(self, text: str) -> ParseTree:
+        return self._parser.parse(text)
+
     def parse_file(self, filepath: str) -> ParseTree:
         with open(filepath, "r") as file:
             return self._parser.parse(file.read())
-
-    def parse(self, kappa_string: str) -> ParseTree:
-        return self._parser.parse(kappa_string)
 
 
 kappa_parser = KappaParser()
