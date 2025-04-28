beffd4ed8dd495ee2ba1af4f0ee10bbb23f53509
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-04-28 09:35:22 -0400

    tidy rule building

--- kappybara/grammar/rule_builder.py
+++ kappybara/grammar/rule_builder.py
@@ -9,7 +9,6 @@ from kappybara.site_states import *
 
 def rule_from_kappa(kappa_str: str) -> Rule:
     rules = rules_from_kappa(kappa_str)
-
     assert len(rules) == 1, "The given rule expression represents more than one rule."
     return rules[0]
 
@@ -21,7 +20,6 @@ def rules_from_kappa(kappa_str: str) -> list[Rule]:
     """
     input_tree = kappa_parser.parse(kappa_str)
     assert input_tree.data == "kappa_input"
-
     rule_tree = input_tree.children[0]
     return RuleBuilder(rule_tree).objects
 
@@ -53,26 +51,21 @@ class RuleBuilder(Visitor):
 
         alg_exp = tree.children[0]
         assert alg_exp.data == "algebraic_expression"
-
         if (
             len(alg_exp.children) > 1
             or not isinstance(alg_exp.children[0], Token)
             or alg_exp.children[0].type != "SIGNED_FLOAT"
         ):
-            raise NotImplementedError(
-                "Generic algebraic expressions not implemented yet. We'll need a full system for parsing/evaluating such expressions."
-            )
+            raise NotImplementedError("Generic algebraic expressions not implemented.")
         else:
-            rate = float(alg_exp.children[0])
-            self.parsed_rates.append(rate)
+            self.parsed_rates.append(float(alg_exp.children[0]))
 
     # Visitor method for Lark
     def rule_expression(self, tree: ParseTree) -> None:
         assert tree.data in ["rule_expression", "rev_rule_expression"]
-        # Find the location of the arrow in the expression
         mid_idx = next(
             (i for i, child in enumerate(tree.children) if child in ["->", "<->"])
-        )
+        )  # Locate the arrow in the expression
 
         for i, child in enumerate(tree.children):
             if i == mid_idx:
@@ -102,31 +95,24 @@ class RuleBuilder(Visitor):
         match self.tree_data:
             case "f_rule":
                 assert len(rates) == 1
-                rule = KappaRule(left, right, rates[0])
-                rules.append(rule)
+                rules.append(KappaRule(left, right, rates[0]))
             case "fr_rule":
                 assert len(rates) == 2
-                rule_f = KappaRule(left, right, rates[0])
-                rule_r = KappaRule(right, left, rates[1])
-                rules.extend([rule_f, rule_r])
+                rules.append(KappaRule(left, right, rates[0]))
+                rules.append(KappaRule(right, left, rates[1]))
             case "ambi_rule":
                 # TODO: check that the order of the rates is right
                 assert len(rates) == 2
                 if rates[0] != 0:
-                    rule_bi = KappaRuleBimolecular(left, right, rates[0])
-                    rules.append(rule_bi)
+                    rules.append(KappaRuleBimolecular(left, right, rates[0]))
                 if rates[1] != 0:
-                    rule_uni = KappaRuleUnimolecular(left, right, rates[1])
-                    rules.append(rule_uni)
+                    rules.append(KappaRuleUnimolecular(left, right, rates[1]))
             case "ambi_fr_rule":
                 assert len(rates) == 3
                 if rates[0] != 0:
-                    rule_bi = KappaRuleBimolecular(left, right, rates[0])
-                    rules.append(rule_bi)
+                    rules.append(KappaRuleBimolecular(left, right, rates[0]))
                 if rates[1] != 0:
-                    rule_uni = KappaRuleUnimolecular(left, right, rates[1])
-                    rules.append(rule_uni)
-                rule_r = KappaRule(right, left, rates[2])
-                rules.append(rule_r)
+                    rules.append(KappaRuleUnimolecular(left, right, rates[1]))
+                rules.append(KappaRule(right, left, rates[2]))
 
         return [r for r in rules if r is not None]
