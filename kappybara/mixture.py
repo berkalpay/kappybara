1c751a780943adefc1d0513c431cef58650208d8
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-05-06 15:13:44 -0400

    add getitem method to agents

--- kappybara/mixture.py
+++ kappybara/mixture.py
@@ -62,9 +62,9 @@ class Mixture:
                 if site.coupled:
                     partner: Site = site.partner
                     i_partner = component.agents.index(partner.agent)
-                    new_agents[i].interface[site.label].partner = new_agents[
-                        i_partner
-                    ].interface[partner.label]
+                    new_agents[i][site.label].partner = new_agents[i_partner][
+                        partner.label
+                    ]
 
         new_component = Component(new_agents, n_copies)
 
@@ -95,11 +95,11 @@ class Mixture:
                     break
 
                 for site_name in a.interface:
-                    a_site = a.interface[site_name]
+                    a_site = a[site_name]
                     if site_name not in b.interface and not a_site.undetermined:
                         search_failed = True
                         break
-                    b_site = b.interface[site_name]
+                    b_site = b[site_name]
 
                     if not a_site.matches(b_site):
                         search_failed = True
