b0a140104e82e92eca714ca514884675f3ecb43e
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-05-20 18:11:30 -0400

    add heterodimerization correctness test

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,19 +1,7 @@
-# For now this is just a test to see if the code runs, and doesn't test anything
-# about correctness of results. In `test_rule.py` there are a few tests with simple
-# models whose progression is entirely deterministic where we check that rule embeddings
-# are being counted correctly, but when we want to test more complex models, see TODO below.
-#
-# TODO:
-# At the level of `System`, when we're running whole models, I think correctness
-# testing should start to look more like statistical tests on distributions over
-# system state and observable counts w.r.t. to empirically known or analytically
-# derived ground truths. This requires the example models/expected results which
-# we're expecting from Walter. On the code side it also will probably end up
-# requiring us to continue the effort to try to support the full Kappa language
-# to make importing/testing these models more feasible. A non-exhaustive list of
-# stuff we still need to implement in that regard:
-# - Algebraic expressions (for example to be used in rate constants, see rule.py and rate.py)
-# - Agent signatures
+import os
+import random
+import pytest
+
 from kappybara.mixture import Mixture
 from kappybara.system import System
 import kappybara.kappa as kappa
@@ -60,3 +48,31 @@ def test_basic_system():
         for obs in observables:
             c = system.count_observable(obs)
             counts[obs].append(c)
+
+
+@pytest.mark.parametrize("k_on, expected", [(2.5e8, 65), (2.5e9, 331)])
+def test_heterodimerization(k_on, expected):
+    random.seed(42)
+    avogadro = 6.0221413e23
+    volume = 2.25e-12  # mammalian cell volume
+    n_a, n_b = 1000, 1000
+    heterodimer = kappa.component("A(x[1]),B(x[1])")
+    system = System(
+        Mixture([kappa.pattern("A(x[.])")] * n_a + [kappa.pattern("B(x[.])")] * n_b),
+        rules=[
+            kappa.rule(
+                f"A(x[.]), B(x[.]) -> A(x[1]), B(x[1]) @ {k_on / (avogadro * volume)}"
+            ),
+            kappa.rule(f"A(x[1]), B(x[1]) -> A(x[.]), B(x[.]) @ {2.5}"),
+        ],
+        observables=[heterodimer],
+    )
+
+    n_heterodimers = []
+    while system.time < 2:
+        system.update()
+        if system.time > 1:
+            n_heterodimers.append(system.count_observable(heterodimer))
+
+    measured = sum(n_heterodimers) / len(n_heterodimers)
+    assert abs(measured - expected) < expected / 5
