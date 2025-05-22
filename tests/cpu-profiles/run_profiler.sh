9022199dd1d89a397ef1c22387b540a3c647f712
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-05-22 17:54:55 -0400

    tidy profiler script

--- tests/cpu-profiles/run_profiler.sh
+++ tests/cpu-profiles/run_profiler.sh
@@ -1,23 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Get the basename of the current directory
-current_dir=$(basename "$PWD")
-required_dir="cpu-profiles"
-
-# Only run if in the tests/cpu-profiles directory
-if [[ "$current_dir" != "cpu-profiles" ]]; then
-    echo "Error: run this in tests/'$required_dir'."
-    exit 1  # Exit with a non-zero status to indicate failure
-fi
-
+cd "$(dirname "$0")"
 mkdir -p results
 
 # Loop through all profile_*.py files in the current directory
 for script_name in *.py; do
-    # Extract the base name (without the .py extension)
-    base_name="${script_name%.py}"
-
-    # Create the corresponding output file
+    base_name="${script_name%.py}"  # Extract the base name
     output_file="results/${base_name}.svg"
 
     if command -v "py-spy" >/dev/null 2>&1; then
