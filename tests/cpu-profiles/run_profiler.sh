3a5d9d9c04f30fc1fd201150ab005075d727e269
amber227 <jyulin2000@protonmail.com>
2025-07-02 21:57:15 -0400

    ci: fix+refactor profiling

--- tests/cpu-profiles/run_profiler.sh
+++ tests/cpu-profiles/run_profiler.sh
@@ -3,15 +3,6 @@
 cd "$(dirname "$0")"
 mkdir -p results
 
-# Loop through all profile_*.py files in the current directory
-for script_name in *.py; do
-    base_name="${script_name%.py}"  # Extract the base name
-    output_file="results/${base_name}.svg"
-
-    if command -v "py-spy" >/dev/null 2>&1; then
-        py-spy record -o ${output_file} -- python3 ${script_name}
-    else
-        # In case we're not in the venv
-        uv run py-spy record -o ${output_file} -- python3 ${script_name}
-    fi
+for script_name in profile_*.py; do
+    python bench.py ${script_name}
 done
