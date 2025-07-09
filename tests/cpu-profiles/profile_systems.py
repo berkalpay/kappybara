d67d0f108c09d53bf93f7638d6e6438beb1a9e54
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-07-09 12:48:27 -0400

    remove unused profile function

--- tests/cpu-profiles/profile_systems.py
+++ tests/cpu-profiles/profile_systems.py
@@ -2,13 +2,7 @@ import kappybara.kappa as kappa
 from kappybara.examples import heterodimerization_system
 
 
-def heterodimerization() -> None:
-    system = heterodimerization_system(2.5e9, kappa.component("A(x[1]),B(x[1])"))
-    while system.time < 2:
-        system.update()
-
-
 if __name__ == "__main__":
     system = heterodimerization_system(2.5e9, kappa.component("A(x[1]),B(x[1])"))
-    while system.time < 2:
+    while system.time < 100:
         system.update()
