62fd3a7cf1aa6911cc1707beabf2062ef2d633ea
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-08-08 11:00:45 -0400

    tidy algebra tests

--- tests/test_algebra.py
+++ tests/test_algebra.py
@@ -3,21 +3,14 @@ import pytest
 import kappybara.kappa as kappa
 
 
-@pytest.mark.parametrize("test_case", [("[max] (1, 2, 4)", 4), ("[min] (1, 2, 4)", 1)])
-def test_list_op(test_case):
-    alg_exp_str, expected = test_case
-    exp_str = f"%obs: 'val' {alg_exp_str}"
-    system = kappa.system(exp_str)
-
-    assert system.observables["val"].evaluate() == expected
-
-
 @pytest.mark.parametrize(
-    "test_case", [("[true] [?] 1 [:] 0", 1), ("[false] [?] 1 [:] 0", 0)]
+    "expression, result",
+    [
+        ("[true] [?] 1 [:] 0", 1),
+        ("[false] [?] 1 [:] 0", 0),
+        ("[max] (1, 2, 4)", 4),
+        ("[min] (1, 2, 4)", 1),
+    ],
 )
-def test_conditional(test_case):
-    alg_exp_str, expected = test_case
-    exp_str = f"%obs: 'val' {alg_exp_str}"
-    system = kappa.system(exp_str)
-
-    assert system.observables["val"].evaluate() == expected
+def test_algexp_evaluation(expression, result):
+    assert kappa.system(f"%obs: 'x' {expression}")["x"] == result
