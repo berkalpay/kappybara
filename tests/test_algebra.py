3dd991a439252a366300c767a77bf13b39334828
amber227 <jyulin2000@protonmail.com>
2025-10-20 17:11:24 -0400

    expression: add unit tests for operator precedence

--- tests/test_algebra.py
+++ tests/test_algebra.py
@@ -13,3 +13,15 @@ from kappybara.system import System
 )
 def test_expression_evaluation(expression, result):
     assert System.from_ka(f"%obs: 'x' {expression}")["x"] == result
+
+
+@pytest.mark.parametrize(
+    "expression, result",
+    [
+        ("1 + 2 * 3", 7),
+        ("1 + 2 * 3 ^ 2", 19),
+        ("2 * 2 ^ 3 * 2", 32),
+    ],
+)
+def test_operator_precedence(expression, result):
+    assert System.from_ka(f"%obs: 'x' {expression}")["x"] == result
