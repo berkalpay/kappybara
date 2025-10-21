138503ea95902bfb4bc2d25910a4f58ab5c1800d
amber227 <jyulin2000@protonmail.com>
2025-10-21 14:50:17 -0400

    adjust grammar to enforce order of operations in evaluating expressions

--- tests/test_algebra.py
+++ tests/test_algebra.py
@@ -13,3 +13,16 @@ from kappybara.system import System
 )
 def test_expression_evaluation(expression, result):
     assert System.from_ka(f"%obs: 'x' {expression}")["x"] == result
+
+
+@pytest.mark.parametrize(
+    "expression, result",
+    [
+        ("1 + 2 * 3", 7),
+        ("1 + 2 * 3 ^ 2", 19),
+        ("2 * 2 ^ 3 * 2", 32),
+        ("'x' + 2 * 'x'", 30),
+    ],
+)
+def test_operator_precedence(expression, result):
+    assert System.from_ka(f"%var: 'x' 10\n%obs: 'y' {expression}")["y"] == result
