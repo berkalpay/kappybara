1ce46cbbeff09a6a20fdf62840cc005244c7eba7
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-10-21 14:46:46 -0400

    add order of operations test involving a variable

--- tests/test_algebra.py
+++ tests/test_algebra.py
@@ -21,7 +21,8 @@ def test_expression_evaluation(expression, result):
         ("1 + 2 * 3", 7),
         ("1 + 2 * 3 ^ 2", 19),
         ("2 * 2 ^ 3 * 2", 32),
+        ("'x' + 2 * 'x'", 30),
     ],
 )
 def test_operator_precedence(expression, result):
-    assert System.from_ka(f"%obs: 'x' {expression}")["x"] == result
+    assert System.from_ka(f"%var: 'x' 10\n%obs: 'y' {expression}")["y"] == result
